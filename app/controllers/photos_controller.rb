b8d3537011459b9a864dcb0daf0cb0c574b61b4a
urayusuke <crfnf690@yahoo.co.jp>
2020-05-24 21:18:36 +0900

    toppage update

--- app/controllers/photos_controller.rb
+++ app/controllers/photos_controller.rb
@@ -1,16 +1,12 @@
 class PhotosController < ApplicationController
 
   def index
-    # @user = User.find(params[:id])
-
   end
   
   def create
-    @user = User.new(params[:id])
   end
 
   def update
-    @user = User.new(params[:id])
   end
 
 end
