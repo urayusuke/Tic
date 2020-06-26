47d3aaa6f2b73a2e9bae733481117a6a81f819cc
urayusuke <crfnf690@yahoo.co.jp>
2020-06-26 17:55:01 +0900

    migrate update

--- db/migrate/20200521135015_create_photos.rb
+++ db/migrate/20200521135015_create_photos.rb
@@ -1,10 +1,10 @@
 class CreatePhotos < ActiveRecord::Migration[5.2]
   def change
     create_table :photos do |t|
+      t.integer :user_id
       t.string :image, null: false
       t.string :text
       t.timestamps
-      t.integer :user_id
     end
     add_index :image, :text, length: 20
   end
